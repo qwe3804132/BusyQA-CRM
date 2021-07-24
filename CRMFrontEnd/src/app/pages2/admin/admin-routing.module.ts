9ba8388f916bc5848f65e4e6edea50f95facbc2c
Peiran Liu <liupeiran9324@outlook.com>
2021-07-24 17:52:18 -0400

    added team component

--- CRMFrontEnd/src/app/pages2/admin/admin-routing.module.ts
+++ CRMFrontEnd/src/app/pages2/admin/admin-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  {
+    path: 'team',
+    loadChildren: () => import('./team/team.module').then((m) => m.TeamModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
---
+++ CRMFrontEnd/src/app/pages2/admin/team/team.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeamRoutingModule } from './team-routing.module';
+import { TeamComponent } from './team.component';
+
+@NgModule({
+  declarations: [TeamComponent],
+  imports: [CommonModule, TeamRoutingModule],
+  exports: [TeamRoutingModule],
+})
+export class TeamModule {}
