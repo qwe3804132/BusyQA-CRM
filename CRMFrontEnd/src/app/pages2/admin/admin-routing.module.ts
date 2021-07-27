c9faa3af96a14b5720e7cb8c3eac9c63dd887164
Peiran Liu <liupeiran9324@outlook.com>
2021-07-26 22:16:34 -0400

    2021/7/26

--- CRMFrontEnd/src/app/pages2/admin/admin-routing.module.ts
+++ CRMFrontEnd/src/app/pages2/admin/admin-routing.module.ts
@@ -1,9 +1,13 @@
+import { TeamComponent } from './team/team.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent },
+  {
+    path: '',
+    component: AdminComponent,
+  },
   {
     path: 'team',
     loadChildren: () => import('./team/team.module').then((m) => m.TeamModule),
